7e237a59a945e8305722d5a15fc0ebad346f4786
泡泡龙 <songlongwind@126.com>
2016-06-16 10:21:02 +0800

    first commit

---
+++ SaberSokoban/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaberSokoban
+//
+//  Created by songlong on 16/6/16.
+//  Copyright © 2016年 SaberGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SaberSokoban/SLMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLMainViewController.h
+//  SaberSokoban
+//
+//  Created by songlong on 16/6/16.
+//  Copyright © 2016年 SaberGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLMainViewController : UIViewController
+
+@end
